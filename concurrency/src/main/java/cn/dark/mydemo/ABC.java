00a11e5d6015f965ae38eb2bf22c5618fe4ecc65
darker <1312603070@qq.com>
2021-04-08 10:26:40 +0800

    add:ABC

--- concurrency/src/main/java/cn/dark/mydemo/ABC.java
+++ concurrency/src/main/java/cn/dark/mydemo/ABC.java
@@ -1,7 +1,7 @@
 package cn.dark.mydemo;
 
-import lombok.extern.slf4j.Slf4j;
-
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -12,15 +12,20 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author dark
  * @date 2020-09-23
  */
-@Slf4j
 public class ABC {
 
     private int sort = 0;
+
+    private volatile int symbo = 0;
 
     private Lock lock = new ReentrantLock();
     private Condition condition1 = lock.newCondition();
     private Condition condition2 = lock.newCondition();
     private Condition condition3 = lock.newCondition();
+
+    private Semaphore semaphore1 = new Semaphore(1);
+    private Semaphore semaphore2 = new Semaphore(0);
+    private Semaphore semaphore3 = new Semaphore(0);
 
     public static void main(String[] args) throws InterruptedException {
         ABC lock4 = new ABC();
@@ -34,15 +39,23 @@ public class ABC {
         //     lock4.test("c", 2, 0);
         // }, "t3").start();
 
-        new Thread(() -> {
-            lock4.test1("a", 0, lock4.condition1, lock4.condition2);
-        }, "t1").start();
-        new Thread(() -> {
-            lock4.test1("b", 1, lock4.condition2, lock4.condition3);
-        }, "t2").start();
-        new Thread(() -> {
-            lock4.test1("c", 2, lock4.condition3, lock4.condition1);
-        }, "t3").start();
+        // new Thread(() -> {
+        //     lock4.test1("a", 0, lock4.condition1, lock4.condition2);
+        // }, "t1").start();
+        // new Thread(() -> {
+        //     lock4.test1("b", 1, lock4.condition2, lock4.condition3);
+        // }, "t2").start();
+        // new Thread(() -> {
+        //     lock4.test1("c", 2, lock4.condition3, lock4.condition1);
+        // }, "t3").start();
+
+        // new Thread(() -> lock4.test2("a", lock4.semaphore1, lock4.semaphore2)).start();
+        // new Thread(() -> lock4.test2("b", lock4.semaphore2, lock4.semaphore3)).start();
+        // new Thread(() -> lock4.test2("c", lock4.semaphore3, lock4.semaphore1)).start();
+
+        new Thread(() -> lock4.test3("a", 0)).start();
+        new Thread(() -> lock4.test3("b", 1)).start();
+        new Thread(() -> lock4.test3("c", 2)).start();
     }
 
     private void test(String msg, int sort, int next) {
@@ -89,6 +102,38 @@ public class ABC {
                 lock.unlock();
             }
 
+        }
+    }
+
+    private void test2(String msg, Semaphore cur, Semaphore next) {
+        for (int i = 0; i < 5; i++) {
+            try {
+                cur.acquire();
+                System.out.print(msg);
+                if (semaphore3 == cur) {
+                    System.out.println();
+                }
+                next.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void test3(String msg, int target) {
+        while (true) {
+            if (this.symbo % 3 == target) {
+                System.out.print(msg);
+                this.symbo++;
+            }
+        }
+    }
+
+    private void test4(String msg, int target, Thread next) {
+        for (int i = 0; i < 4; i++) {
+            if (this.sort % 3 == target) {
+
+            }
         }
     }
 
